0e121031c1946d9587d4e897626d54bfe433ed5e
automationblog <sawin24@gmail.com>
2024-10-14 17:27:02 +0530

    connect DB added

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import Dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./dbUtils/connectDB.js";
+
+//Create express app
+const app = express();
+
+//Middlewares
+app.use(cors());
+Dotenv.config();
+app.use(express.json());
+
+//Home Page Route
+app.get("/", (req, res) => {
+  res.send("Nots APP Backend API");
+});
+
+//Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log("Server started on port " + PORT);
+  console.log("http://localhost:" + PORT);
+});
